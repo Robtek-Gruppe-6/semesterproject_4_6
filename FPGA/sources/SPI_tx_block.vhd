8d18fe44548a355b723168df16451f2e9a18a994
Teh-Faber <159794966+Teh-Faber@users.noreply.github.com>
2025-04-10 13:47:02 +0200

    Fixed SPI TX
    and removed redundant code

--- FPGA/sources/SPI_tx_block.vhd
+++ FPGA/sources/SPI_tx_block.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 20.03.2025 10:36:47
+-- Create Date: 09.04.2025 11:59:12
 -- Design Name: 
--- Module Name: SPI_tx_block - Behavioral
+-- Module Name: shifting_register - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -32,25 +32,54 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity SPI_tx_block is
+    generic( BUS_WIDTH : positive := 8;
+             dir : STD_LOGIC := '0'
+    );
     Port ( clk : in STD_LOGIC;
-           sent_bit : out STD_LOGIC;
-           en, rst : in STD_LOGIC;
-           bit_to_send : in STD_LOGIC);
+           data_in : in STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0);
+           en : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           bit_out, done : out STD_LOGIC := '0');
 end SPI_tx_block;
 
 architecture Behavioral of SPI_tx_block is
-
+    signal shifting_reg : STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0) := (others => '0');
+    signal shift_cnt    : UNSIGNED (BUS_WIDTH-1 downto 0) := (others => '0');
 begin
     process(clk, rst)
     begin
         if (rst = '1') then
-            sent_bit  <= '0';
+            bit_out <= '0';
+            done <= '0';
+            shifting_reg <= (others => '0');
+            shift_cnt <= (others => '0');
         else
             if rising_edge(clk) then
                 if en = '1' then
-                    sent_bit <= bit_to_send;
+                    -- reset done
+                    done <= '0';
+                    
+                    if (dir = '1') then
+                        -- shift left, So MSB sent first
+                        bit_out <= shifting_reg(BUS_WIDTH -1);
+                        shifting_reg <= shifting_reg(BUS_WIDTH - 2 downto 0) & '0';
+                        -- count for done signal
+                        shift_cnt <= shift_cnt+1;
+                    else
+                        -- shift right, So LSB sent first
+                        bit_out <= shifting_reg(0);     
+                        shifting_reg <= '0' &shifting_reg(BUS_WIDTH - 1 downto 1);
+                        -- count for done signal
+                        shift_cnt <= shift_cnt+1;
+                    end if;
+                    
+                    if (shift_cnt >= BUS_WIDTH) then
+                        -- reset count and output done
+                        shift_cnt <= (others => '0');
+                        done <= '1';
+                    end if;
                 end if;
-            end if;
+            end if;    
         end if;
     end process;
 end Behavioral;
