1b110cba4e7fd3c46ff86f18eac8ad037f373511
Teh-Faber <159794966+Teh-Faber@users.noreply.github.com>
2025-04-30 15:38:04 +0200

    SPI_TX virker

--- FPGA/sources/SPI_tx_block.vhd
+++ FPGA/sources/SPI_tx_block.vhd
@@ -18,7 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -36,50 +35,78 @@ entity SPI_tx_block is
              dir : STD_LOGIC := '0'
     );
     Port ( clk : in STD_LOGIC;
-           data_in : in STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0);
-           en : in STD_LOGIC;
+           data_to_send : in STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0);
+           ss : in STD_LOGIC;
            rst : in STD_LOGIC;
-           bit_out, done : out STD_LOGIC := '0');
+           clk_spi, miso, busy : out STD_LOGIC := '0');
 end SPI_tx_block;
 
 architecture Behavioral of SPI_tx_block is
-    signal shifting_reg : STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0) := (others => '0');
-    signal shift_cnt    : UNSIGNED (BUS_WIDTH-1 downto 0) := (others => '0');
+    signal busy_sig, busy_latch : STD_LOGIC := '0';
 begin
     process(clk, rst)
+        variable shifting_reg : STD_LOGIC_VECTOR (BUS_WIDTH-1 downto 0) := (others => '0');
+        variable shift_cnt    : UNSIGNED (BUS_WIDTH-1 downto 0) := (others => '0');
     begin
         if (rst = '1') then
-            bit_out <= '0';
-            done <= '0';
-            shifting_reg <= (others => '0');
-            shift_cnt <= (others => '0');
+            clk_spi <= '0';
+            miso <= '0';
+            busy_sig <= '0';
+            shifting_reg := (others => '0');
+            shift_cnt := (others => '0');
         else
-            if rising_edge(clk) then
-                if en = '1' then
-                    -- reset done
-                    done <= '0';
-                    
-                    if (dir = '1') then
-                        -- shift left, So MSB sent first
-                        bit_out <= shifting_reg(BUS_WIDTH -1);
-                        shifting_reg <= shifting_reg(BUS_WIDTH - 2 downto 0) & '0';
-                        -- count for done signal
-                        shift_cnt <= shift_cnt+1;
-                    else
-                        -- shift right, So LSB sent first
-                        bit_out <= shifting_reg(0);     
-                        shifting_reg <= '0' &shifting_reg(BUS_WIDTH - 1 downto 1);
-                        -- count for done signal
-                        shift_cnt <= shift_cnt+1;
+            -- update clk_spi
+            if (busy_sig = '1' and ss = '1')  then
+                clk_spi <= clk;
+				else
+				clk_spi <= '0';
+            end if;
+            
+            if falling_edge(clk) then
+                if ss = '1' then
+                    -- send data only if shifting_reg is filled
+                    if (busy_latch = '1') then
+                        if (dir = '1') then
+                            -- shift left, So MSB sent first
+                            miso <= shifting_reg(BUS_WIDTH -1 -TO_INTEGER(shift_cnt));
+    
+                            -- count for done signal
+                            shift_cnt := shift_cnt+1;
+                        else
+                            -- shift right, So LSB sent first
+                            miso <= shifting_reg(TO_INTEGER(shift_cnt));     
+                            
+                            -- count for done signal
+                            shift_cnt := shift_cnt+1;
+                        end if;
+                    end if;
+                
+                    -- fill shifting_reg if not busy
+                    if (busy_latch = '0') then
+                        shifting_reg := data_to_send;
+                        -- set busy
+                        busy_latch <= '1'; 
+                        miso <= '0'; 
                     end if;
                     
+                    -- delay busy ouput by 1 cycle
+                     busy_sig <= busy_latch;
+                     
+                    -- reset count when done and busy signal
                     if (shift_cnt >= BUS_WIDTH) then
-                        -- reset count and output done
-                        shift_cnt <= (others => '0');
-                        done <= '1';
+                        -- reset count and busy
+                        shift_cnt := (others => '0');
+                    end if;
+                    
+                    -- set busy low 1 cycle after shift cnt has been reset
+                    if (shift_cnt <= 0 and busy_sig = '1') then
+                        busy_latch <= '0';                  
                     end if;
                 end if;
             end if;    
         end if;
     end process;
+    
+    busy <= busy_sig;
+    
 end Behavioral;
