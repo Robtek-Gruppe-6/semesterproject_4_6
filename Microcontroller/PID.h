26432ba896e72a59421cf95d0130ef9272876ef7
PapaEchoRomeo <lukasvadmand@gmail.com>
2025-05-14 14:46:43 +0200

    added PID files
    just layout setup

---
+++ Microcontroller/PID.h
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * University of Southern Denmark
+ * Embedded C Programming (ECP)
+ *
+ * MODULENAME.: PID.h
+ *
+ * PROJECT....: SEMESTERPROJECT
+ *
+ * DESCRIPTION: Header file for PID controller module.
+ *
+ * Change Log:
+ ******************************************************************************
+ * Date    Id    Change
+ * YYMMDD
+ * --------------------
+ * 250514  <YourID>    Module created.
+ *
+ *****************************************************************************/
+
+#ifndef PID_H_
+#define PID_H_
+
+/***************************** Include files ********************************/
+#include <stdint.h>
+
+/*****************************    Defines    ********************************/
+
+/*****************************   Constants   ********************************/
+
+/*****************************   Functions   ********************************/
+
+/* Example function prototype:
+void PID_Init(void);
+*/
+
+/****************************** End Of Module ********************************/
+#endif /* PID_H_ */
