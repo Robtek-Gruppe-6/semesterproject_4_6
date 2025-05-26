d0bcd3007ca92d2c88840cf30f6d8c69fe26dbc8
spkki <Svend.b.t@hotmail.dk>
2025-05-26 12:29:22 +0200

    Update busdmux.vhd

--- FPGA/sources/busdmux.vhd
+++ FPGA/sources/busdmux.vhd
@@ -7,20 +7,33 @@ entity busdmux is
         BUS_WIDTH : integer := 8
     );
     port (
-        data_in   : in  std_logic_vector(BUS_WIDTH - 1 downto 0);
-        sel       : in  std_logic_vector(0 downto 0);
+        clk, en     : in std_logic;
+        data_in     : in  std_logic_vector(BUS_WIDTH - 1 downto 0);
+        sel         : in  std_logic_vector(0 downto 0);
+        rst         : in std_logic;
         data_out_1, data_out_2 : out STD_LOGIC_VECTOR(BUS_WIDTH - 1 downto 0) := (others => '0')
     );
 end entity;
 
 architecture Behavioral of busdmux is
+    signal data_sig_1, data_sig_2 : STD_LOGIC_VECTOR(BUS_WIDTH - 1 downto 0) := (others => '0');
 begin
     process(data_in, sel)
-        begin
+    begin
+        if rst = '1' then
+            data_sig_1 <= (others => '0');
+            data_sig_2 <= (others => '0');
+        elsif rising_edge(clk) and en = '1' then
+            data_sig_1 <= data_sig_1;
+            data_sig_2 <= data_sig_2;
             if(sel = "1") then
-                data_out_2 <= data_in;
-            else
-                data_out_1 <= data_in;
+                data_sig_2 <= data_in;
+            elsif (sel = "0") then
+                data_sig_1 <= data_in;
             end if;
+        end if;
     end process;
+    
+    data_out_1 <= data_sig_1;
+    data_out_2 <= data_sig_2;
 end architecture;
